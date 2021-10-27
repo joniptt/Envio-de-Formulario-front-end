78e1860c947b79d06a083c930301ff3463268a65
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2021-10-26 23:29:48 -0300

    update axios

--- ProjCad/cadastro/src/boot/axios.js
+++ ProjCad/cadastro/src/boot/axios.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import axios from 'axios'
+import axios from "axios";
+import Vue from "vue";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 5000,
-  });
-  
+  baseURL: "http://localhost:3000",
+  timeout: 5000
+});
 
-Vue.prototype.$axios = axiosInstance
+Vue.prototype.$axios = axiosInstance;
 
-export { axiosInstance }
+export { axiosInstance };
