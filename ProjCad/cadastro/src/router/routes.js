8e1530c1147550e209f8ed3fee6d145eab109c49
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2021-11-03 16:35:11 -0300

    Update routes.js

--- ProjCad/cadastro/src/router/routes.js
+++ ProjCad/cadastro/src/router/routes.js
@@ -1,20 +1,19 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '/', component: () => import('src/pages/Login.vue') },
-      { path: '/teste', component: () => import('pages/Teste.vue') }
+      { path: "/", component: () => import("src/pages/Login.vue") },
+      { path: "/teste", component: () => import("pages/Teste.vue") }
     ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '*',
-    component: () => import('pages/Error404.vue')
+    path: "*",
+    component: () => import("pages/Error404.vue")
   }
-]
+];
 
-export default routes
+export default routes;
